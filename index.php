60c9663332ab39bcb14c2ce401731780e1146e91
Yunan Chaerurrachman <yunanchaerurrachman7@gmail.com>
2021-01-16 05:22:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime iure tempore, labore rem, et alias cum laboriosam repudiandae nam ea eligendi error delectus, aut rerum dicta natus. Sint, cupiditate ex.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
